013ca78cdc6a285f1b020c325f85654b73b32bab
Djapamal <46869703+AlexisL74@users.noreply.github.com>
2025-01-31 21:26:37 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "config.h"
+
+int main() {
+    std::cout << "Project Albireo Version "
+              << PROJECT_VERSION_MAJOR << "."
+              << PROJECT_VERSION_MINOR << "."
+              << PROJECT_VERSION_PATCH << std::endl;
+    return 0;
+}
